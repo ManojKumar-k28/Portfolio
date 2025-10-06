ca078cf7e290e53880dfc1dab35aa39f5bbde8f2
Manoj Kumar K <kmanojkumar2874@gmail.com>
2025-10-06 16:01:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  // Add the port number to your server configuration
+  server: {
+    host: true, // Exposes the server to your network
+    port: 3001, // <-- SET YOUR DESIRED PORT NUMBER HERE
+  },
+
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
